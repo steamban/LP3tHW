f02b700fa7acc7465e7f325ba6b8fc6d1c030b04
Arvind <nairarvind2011@gmail.com>
2023-06-05 23:54:08 +0530

    adds exercise 48

---
+++ ex48/skeleton/ex48/lexicon.py
@@ -0,0 +1,28 @@
+lexicon = {
+    "north":"direction",
+    "south":"direction",
+    "east":"direction",
+    "west":"direction",
+    "go":"verb",
+    "kill":"verb",
+    "eat":"verb",
+    "the":"stop",
+    "of":"stop",
+    "in":"stop",
+    "bear":"noun",
+    "princeness":"noun",
+    "3":"number",
+    "91234":"number",
+    '1234':"number",
+    "ASDFADFASDF":"error",
+    "IAS":"error"
+}
+
+def scan(sentence):
+    results = []
+    words = sentence.split()
+    for word in words:
+        word_type = lexicon.get(word)
+        results.append((word_type,word))
+    return results
+
